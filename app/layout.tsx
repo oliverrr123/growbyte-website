ab4154667d1125735ca4422212fdd70b76b097b3
oliverrr123 <oliver.cingl@gmail.com>
2025-07-07 22:35:57 -0700

    feat: way better design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "GrowByte",
@@ -19,6 +20,14 @@ export default function RootLayout({
         <main className="w-full p-4 sm:px-[10vw] md:px-[20vw] lg:px-[30vw] xl:px-[30vw] 2xl:px-[35vw]">
           {children}
         </main>
+        <footer>
+          <div className="flex justify-center gap-4 pt-16">
+              <a href="https://github.com/oliverrr123/" target="_blank" aria-label="Github profile"><Image src="/icons/github.svg" alt="GitHub" className="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity" width={32} height={32} /></a>
+              <a href="https://linkedin.com/in/olivercingl/" target="_blank" aria-label="Linkedin profile"><Image src="/icons/linkedin.svg" alt="LinkedIn" className="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity" width={32} height={32} /></a>
+              <a href="mailto:hello@growbyte.co" target="_blank" aria-label="Email"><Image src="/icons/mail.svg" alt="Email" className="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity" width={32} height={32} /></a>
+          </div>
+          <h6 className="py-6 text-center">© 2025 GrowByte</h6>
+      </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,103 @@
 import Image from "next/image";
 
 export default function Home() {
+  let skills: { title: string; description: string; image: string; alt: string; link: string; }[] = [
+      {
+          title: 'Custom solutions',
+          description: 'From building fun websites for myself to creating complex systems for companies, I have the skills to create pretty much any site you could imagine.',
+          // image: 'growbyte-website-screenshot.png',
+          image: 'gradient-1.jpg',
+          alt: 'Screenshot of a website I built for GrowByte company',
+          link: 'web-development',
+      },
+      {
+          title: 'Saas apps',
+          description: 'My hobby of filming cars grew into a whole production company making marketing videos, music clips, car edits, or other videos & photos of any kind.',
+          // image: 'music-video-screenshot.png',
+          image: 'gradient-2.jpg',
+          alt: 'Screenshot of a music video I produced for Mark Steiner',
+          link: 'media-production',
+      },
+      {
+          title: 'Healthcare robot',
+          description: 'Since IT education in traditional schools is usually very poor, I decided to make fun workshops for children in elementary schools. I also teach teachers or whole companies.',
+          // image: 'it-education-workshop.png',
+          image: 'gradient-3.jpg',
+          alt: 'Picture from my IT education workshop',
+          link: 'it-education',
+      }
+  ]
+  let projects: { title: string; image: string; alt: string; link: string; }[] = [
+      {
+          title: 'Luma production agency',
+          // image: 'luma-production-website-screenshot.png',
+          image: 'gradient-4.jpg',
+          alt: 'Screenshot of a website I built for Luma production agency',
+          link: 'https://luma-production.com/'
+      },
+      {
+          title: 'SX Media - car photos & videos',
+          // image: 'sx-media-website-screenshot.png',
+          image: 'gradient-1.jpg',
+          alt: 'Screenshot of a website I built for SX Media',
+          link: 'https://sx-media.vercel.app/'
+      },
+      {
+          title: 'GrowByte website',
+          // image: 'growbyte-website-screenshot.png',
+          image: 'gradient-2.jpg',
+          alt: 'Screenshot of a website I built for Growbyte company',
+          link: 'https://growbyte.co/'
+      }
+  ]
+
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center w-full mt-24 px-12">
-        <Image src="/growbyte-logo.png" alt="logo" width={400} height={400} />
-        <h2 className="text-lg">Making you more money with <span className="font-semibold text-primary">AI</span></h2>
-      </div>
-      <div className="flex flex-col w-full mt-24 gap-16">
-        <h3 className="text-3xl font-black text-center">Custom Solutions</h3>
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col gap-4">
-            <Image src="/gradient-1.jpg" alt="website" className="rounded-lg w-full" width={1000} height={1000} />
-            <h2 className="text-2xl font-bold">Websites</h2>
-            <ul className="list-disc list-inside">
-              <li><a href="https://sko.vercel.app" className="text-primary">SKO</a></li>
-              <li><a href="https://konzultace.vercel.app" className="text-primary">Martin Krejčíř - Consultations</a></li>
-              <li><a href="https://martin-krejcir-business-plan.vercel.app" className="text-primary">Martin Krejčíř - Business plan</a></li>
-              <li><a href="https://lucislavikova.vercel.app" className="text-primary">Life Coach Luci Slavíková</a></li>
-            </ul>
-          </div>
-          <div className="flex flex-col gap-4">
-            <Image src="/gradient-2.jpg" alt="website" className="rounded-lg w-full" width={1000} height={1000} />
-            <h2 className="text-2xl font-bold">AI Automations</h2>
-            <ul className="list-disc list-inside">
-              <li><a href="https://sko.vercel.app">Automated Cold Outreach</a></li>
-              <li><a href="https://konzultace.vercel.app">Automated Website Blogs</a></li>
-              <li><a href="https://martin-krejcir-business-plan.vercel.app">Customer Support AI Chatbot</a></li>
-            </ul>
-          </div>
-        </div>
-        <div className="flex flex-col w-full mt-8 gap-8">
-          <h3 className="text-3xl font-black text-center">Apps</h3>
-          <div className="flex flex-col gap-8">
-            <div className="flex flex-col gap-2">
-              <Image src="/gradient-3.jpg" alt="website" className="rounded-lg w-full" width={1000} height={1000} />
-              <h2 className="text-2xl font-bold">IG Byte</h2>
-              <p>MVP: <a className="text-primary" href="https://growbyte.cz">growbyte.cz</a></p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <Image src="/gradient-4.jpg" alt="website" className="rounded-lg w-full" width={1000} height={1000} />
-              <h2 className="text-2xl font-bold">HR Byte</h2>
-              <p>Design</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <p className="text-center text-sm opacity-75 mt-12">&copy; 2025 GrowByte</p>
+    <>
+
+    <div className="flex w-full justify-center items-center pt-24 pb-6">
+        <div className="w-48 h-48 bg-primary rounded-full"></div>
     </div>
+    <div className="flex w-full justify-center items-center tracking-[-0.075em] pb-0.5">
+      <h1 className="text-7xl font-black text-black">grow</h1>
+      <h1 className="text-7xl font-black italic text-primary -ml-1.25">byte</h1>
+    </div>
+    <h3 className="text-center text-black text-opacity-80 text-lg font-light ml-16">~ Making you more money with <span className="font-semibold text-primary">AI</span></h3>
+    <h2 className="text-3xl font-semibold text-center pt-24 pb-12">What do we build?</h2>
+    <section className="w-full flex flex-col justify-center gap-20">
+        {skills.map((skill) => (
+            <div key={skill.title} className="flex flex-col gap-2">
+                <Image src={`/images/${skill.image}`} alt={skill.alt} className="w-full rounded-xl" width={1000} height={1000} />
+                <h3 className="text-black font-bold text-lg mt-2">{skill.title}</h3>
+                <p className="text-black text-opacity-75 pb-1">{skill.description}</p>
+                <a href={skill.link}><button className="font-semibold bg-black text-white cursor-pointer rounded-lg px-4 py-1.5">Find out more</button></a>
+            </div>
+        ))}
+    </section>
+
+
+
+{/* 
+        Custom Solutions:
+
+          Websites:
+
+            SKO - https://sko.vercel.app
+            Martin Krejčíř - Consultations - https://konzultace.vercel.app
+            Martin Krejčíř - Business plan - https://martin-krejcir-business-plan.vercel.app
+            Life Coach Luci Slavíková - https://lucislavikova.vercel.app
+
+          AI Automations
+
+            Automated Cold Outreach
+            Automated Website Blogs
+            Customer Support AI Chatbot
+
+        IG Byte
+          MVP: growbyte.cz
+
+        HR Byte
+          Design
+*/}
+    </>
   );
 }
