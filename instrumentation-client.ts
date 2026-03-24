7dd5e84f3e04e6b150a8831c2c0a968fbf01ae4a
oliverrr123 <oliver.cingl@gmail.com>
2026-03-24 15:44:51 +0100

    fix: build error

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,5 +2,5 @@ import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: '2026-01-30'
+  defaults: '2025-11-30'
 })
